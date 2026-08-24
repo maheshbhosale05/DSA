d7d7758e356c427c6475c8c7dd0a0c49fbcb44e8
Mahesh Bhosale <maheshbhosale0507@gmail.com>
2026-08-24 18:06:08 +0200

    Fix bubble sort loop condition and indexing

--- src/Sorting/Bubble.java
+++ src/Sorting/Bubble.java
@@ -13,7 +13,7 @@ public class Bubble {
     public static void bubbleSort(int[] arr){
         for(int i=arr.length-1; i>=1; i--){
             boolean isSwapDid = false;
-            for(int j=0; j<=arr.length-2 ;j++){
+            for(int j=0; j<=i;j++){
                 if(arr[j]>arr[j+1]){
                     isSwapDid = true;
                     Sorting.swap(arr,j,j+1);
